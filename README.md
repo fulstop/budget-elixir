748de54fee0ef5fc141f3afcd7bbba5e3b4d1d7a
Jory Hatton <joryhatton@gmail.com>
2017-03-31 15:17:21 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Budget
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed as:
+
+  1. Add `budget` to your list of dependencies in `mix.exs`:
+
+    ```elixir
+    def deps do
+      [{:budget, "~> 0.1.0"}]
+    end
+    ```
+
+  2. Ensure `budget` is started before your application:
+
+    ```elixir
+    def application do
+      [applications: [:budget]]
+    end
+    ```
+
---
+++ lib/budget.ex
@@ -0,0 +1,57 @@
+defmodule Budget do
+
+  alias NimbleCSV.RFC4180, as: CSV
+
+  def list_transactions do
+    File.read!("lib/transactions.csv")
+      |> parse
+      |> filter
+      |> normalize
+      |> sort
+      |> pretty_print
+  end
+
+  ## private
+
+  defp parse(transactions_string) do
+    transactions_string
+      |> String.replace("\r", "")
+      |> CSV.parse_string
+  end
+
+  defp filter(rows) do
+    Enum.map(rows, &Enum.drop(&1, 1))
+  end
+
+  defp normalize(rows) do
+    Enum.map(rows, &parse_amount(&1))
+  end
+
+  defp parse_amount([date, description, amount]) do
+    [date, description, parse_to_float(amount)]
+  end
+
+  defp parse_to_float(string) do
+    string
+      |> String.to_float
+  end
+
+  defp sort(rows) do
+    Enum.sort(rows, &sort_asc_by_amount(&1, &2))
+  end
+
+  defp sort_asc_by_amount([_,_,prev], [_,_,next]) do
+    prev < next
+  end
+
+  def pretty_print(rows) do
+    IO.puts "\Transactions:"
+    Enum.each(rows, &print_to_console(&1))
+  end
+
+  defp print_to_console([date, description, amount]) do
+    formatted_amount = :erlang.float_to_binary(amount, decimals: 2)
+    IO.puts "#{date} #{description} \t$#{formatted_amount}"
+  end
+
+end
